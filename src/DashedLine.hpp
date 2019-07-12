d1df562e29442af06fb2abe3433a39eb2d25401a
skiprox <seanmichaelscanlan@gmail.com>
2019-07-11 22:50:33 -0400

    Updating with latest dashed line work

--- src/DashedLine.hpp
+++ src/DashedLine.hpp
@@ -12,17 +12,20 @@ class DashedLine
 public:
 
 	DashedLine();
-    DashedLine(glm::vec2 _start, glm::vec2 _end, ofColor _color, int _incMax);
+	DashedLine(glm::vec2 _start, glm::vec2 _end, ofColor _color, int _incMax);
 
-    void setup();
+	void setup();
 	void update(int _inc);
 	void draw();
 
 	ofPath path;
 
-    glm::vec2 start, end;
+	glm::vec2 start, end;
 
-    int inc, incMax;
+	int inc, incMax;
+
+	int desiredStrokeLen = 20;
+	int numSegments;
 
 	ofColor color;
 };
